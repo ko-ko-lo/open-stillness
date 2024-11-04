bc230cc618ebf2fb7ee4ad8301eafa31095824e3
ko-ko-lo <d-kolodzey@web.de>
2024-11-04 22:04:12 +0100

    Added 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LibraryComponent } from './pages/library/library.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { RoutinesComponent } from './pages/routines/routines.component';
 import { YogaRoutinesComponent } from './pages/yoga-routine/yoga-routine.component';
 
@@ -10,4 +11,6 @@ export const routes: Routes = [
   { path: 'routines', component: RoutinesComponent },
 
   { path: 'yoga-routines/:slug', component: YogaRoutinesComponent },
+
+  { path: '**', component: NotFoundComponent },
 ];
