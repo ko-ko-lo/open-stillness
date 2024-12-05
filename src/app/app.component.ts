503efa2d6cb6ce6beb34c7fe2c7a0f2b66816df3
ko-ko-lo <d-kolodzey@web.de>
2024-12-05 23:37:01 +0100

    Fix: Added missing files and updated breathing techniques library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ExploreRoutinesComponent } from './components/explore-routines/explore-routines.component';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { MainMenuComponent } from './core/components/main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    FooterComponent,
-    ExploreRoutinesComponent,
-    MainMenuComponent,
-  ],
+  imports: [RouterOutlet, FooterComponent, MainMenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'kokolo-yoga';
