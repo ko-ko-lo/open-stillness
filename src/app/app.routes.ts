f6ffa9d1550c4d77e7054d53573760e8955e1e23
ko-ko-lo <d-kolodzey@web.de>
2024-12-06 15:14:59 +0100

    Preparing breathing data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { BreathingLibraryComponent } from './features/breathing/library/library.component';
+import { BreathingRoutinesOverviewComponent } from './features/breathing/routines-overview/routines-overview.component';
 import { HomeComponent } from './features/yin-style-yoga/home/home.component';
 import { LibraryComponent } from './features/yin-style-yoga/library/library.component';
 import { YogaRoutinesComponent } from './features/yin-style-yoga/routine/yoga-routine.component';
@@ -11,6 +12,10 @@ export const routes: Routes = [
   { path: 'yin-style-yoga/library', component: LibraryComponent },
   { path: 'yin-style-yoga/routines-overview', component: RoutinesComponent },
   { path: 'breathing/library', component: BreathingLibraryComponent },
+  {
+    path: 'breathing/routines-overview',
+    component: BreathingRoutinesOverviewComponent,
+  },
 
   { path: 'routine/:slug', component: YogaRoutinesComponent },
 
